2e9d7fccab96903c49e332cf9ebff1a57731cbe4
marcuskeller <v.marcusk@gmail.com>
2026-06-05 10:58:28 -0300

    feat: consolidate .gitignore files and update image processing UI

--- image-consumer-ec2/src/main/java/consumer/ImageProcessor.java
+++ image-consumer-ec2/src/main/java/consumer/ImageProcessor.java
@@ -6,7 +6,6 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Base64;
 
 public class ImageProcessor {
     private static final int LARGURA_ALVO = 800;
